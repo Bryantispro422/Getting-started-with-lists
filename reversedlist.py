8180c310b5def21091a18ee8e26a32a00987ae33
Bryantispro422 <cnbeetseh@gmail.com>
2025-03-14 21:05:49 +0100

    revcommit

---
+++ reversedlist.py
@@ -0,0 +1,13 @@
+numbers=[2,6,9,12,16,10,30]
+
+print(numbers)
+
+students=["Kelvin","Dan","Clare","Eric","Clement"]
+
+print(students)
+
+print("My student's dad is called",students[4])
+
+print(students[0],"is my friend.")
+
+print("I am here with",students[1],"and",students[3])
